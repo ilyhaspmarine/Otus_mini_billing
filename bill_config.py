bc978268e5ea27abd9995d8b0dcd9d3fcf6f45f4
ilyhaspmarine <timkaa525@yandex.ru>
2026-02-08 12:01:34 +0300

    В кластер

--- bill_config.py
+++ bill_config.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).parent
 
 class DbSettings(BaseModel):
     driver: str = "postgresql+" + os.getenv("DB_DRIVER_ASYNC", "asyncpg")
-    username: str = os.getenv("DB_BILLING", "username")
+    username: str = os.getenv("DB_USER", "username")
     password: str = os.getenv("DB_PASSWORD", "password")
     host: str = os.getenv("DB_HOST", "host.docker.internal")
     port: str = os.getenv("DB_PORT", "5432")
